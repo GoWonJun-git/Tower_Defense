dd1a00ff65cdca3722a91e611c1f2a1a40428257
DESKTOP-EPSEHON\Jun <zmfpdydgo@secrettown.co.kr>
2024-10-23 15:55:00 +0900

    - 유니티 버전업 - 빌드 에러 수정 - 기타 UI 및 에러 수정

--- Tower_Defense/Assets/Scripts/UI/NodeUI.cs
+++ Tower_Defense/Assets/Scripts/UI/NodeUI.cs
@@ -7,7 +7,7 @@ using UnityEngine.UI;
 public class NodeUI : MonoBehaviour
 {
     public GameObject ui;            // 활성화 / 비활성화 될 UI.
-    private Node target;             // UI가 활성화될 Node.
+    //private Node target;             // UI가 활성화될 Node.
 
     public Text upgradeCost;         // Upgrade 비용.
     public Text sellAmount;          // Sell 비용.
@@ -18,48 +18,48 @@ public class NodeUI : MonoBehaviour
     public Description description;  // Description 객체.
 
     // UI를 활성화할 Node 지정.
-    public void SetTarget(Node _target)
-    {
-        // target 지정.
-        target = _target;
+    //public void SetTarget(Node _target)
+    //{
+    //    // target 지정.
+    //    target = _target;
 
-        // target의 위치를 Build될 포탑의 위치로 설정.
-        transform.position = target.GetBuildPosition();
+    //    // target의 위치를 Build될 포탑의 위치로 설정.
+    //    transform.position = target.GetBuildPosition();
         
-        // Upgrade 단계가 0인 경우.
-        if (target.isUpgradeed == 0)
-        {
-            // Upgrade 비용 지정.
-            upgradeCost.text = "$ " + target.turretBluePrint.cost_2;
-            // Upgrade Button 클릭 가능.
-            upgradeButton.interactable = true;
-        }
-        // Upgrade 단계가 1인 경우.
-        else if (target.isUpgradeed == 1)
-        {
-            // Upgrade 비용 지정.
-            upgradeCost.text = "$ " + target.turretBluePrint.cost_3;
-            // Upgrade Button 클릭 가능.
-            upgradeButton.interactable = true;
-        }
-        // Upgrade 이후인 경우.
-        else
-        {
-            // Upgrade 금지.
-            upgradeCost.text = "DONE";
-            // Upgrade Button 클릭 불가능.
-            upgradeButton.interactable = false;
-        }
+    //    // Upgrade 단계가 0인 경우.
+    //    if (target.isUpgradeed == 0)
+    //    {
+    //        // Upgrade 비용 지정.
+    //        upgradeCost.text = "$ " + target.turretBluePrint.cost_2;
+    //        // Upgrade Button 클릭 가능.
+    //        upgradeButton.interactable = true;
+    //    }
+    //    // Upgrade 단계가 1인 경우.
+    //    else if (target.isUpgradeed == 1)
+    //    {
+    //        // Upgrade 비용 지정.
+    //        upgradeCost.text = "$ " + target.turretBluePrint.cost_3;
+    //        // Upgrade Button 클릭 가능.
+    //        upgradeButton.interactable = true;
+    //    }
+    //    // Upgrade 이후인 경우.
+    //    else
+    //    {
+    //        // Upgrade 금지.
+    //        upgradeCost.text = "DONE";
+    //        // Upgrade Button 클릭 불가능.
+    //        upgradeButton.interactable = false;
+    //    }
 
-        // Sell 비용 지정.
-        sellAmount.text = "$ " + target.turretBluePrint.GetSellAmount(target.isUpgradeed);
+    //    // Sell 비용 지정.
+    //    sellAmount.text = "$ " + target.turretBluePrint.GetSellAmount(target.isUpgradeed);
 
-        // UI 활성화.
-        ui.SetActive(true);
+    //    // UI 활성화.
+    //    ui.SetActive(true);
 
-        // 타워 설명창 정보 갱신.
-        description.Tower_Description_Button_Toggle(target.turret);
-    }
+    //    // 타워 설명창 정보 갱신.
+    //    description.Tower_Description_Button_Toggle(target.turret);
+    //}
 
     // UI 비활성화.
     public void Hide()
@@ -75,10 +75,10 @@ public class NodeUI : MonoBehaviour
         upgradeSound.Play();
 
         // Upgrade 함수 호출.
-        target.TurretUpgrade();
+        //target.TurretUpgrade();
 
         // Node UI 비활성화.
-        BuildManager.instance.DeselectNode();
+        //BuildManager.instance.DeselectNode();
     }
 
     // 타워 Sell.
@@ -88,10 +88,10 @@ public class NodeUI : MonoBehaviour
         sellSound.Play();
 
         // Sell 함수 호출.
-        target.TurretSell();
+        //target.TurretSell();
 
         // Node UI 비활성화.
-        BuildManager.instance.DeselectNode();
+        //BuildManager.instance.DeselectNode();
     }
 
 }
